9e796645a416798129aeb43e7373216dfc02492d
omerahmer <omerahmer918@gmail.com>
2024-07-23 15:03:40 -0700

    fixed index.ts

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const http = require('http');
 
-const dir = "/home/n/na/nanotech/nano-website/server/static";
+const dir = "/home/n/na/nanotech/nano-webapp-new/server/static";
 const sock = process.env.PORT || '/srv/apps/$USER/$USER.sock';
 
 app.use(cors());
